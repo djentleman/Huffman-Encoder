b5a0f02c46e5a8a67aa033fa7d9e7c02bd5343ce
Todd Perry <up612136@myport.ac.uk>
2013-02-17 13:30:19 +0000

    generateCharacterList written
    new function written, takes in list of chr, returns list of Characters
    also prints them out

--- Huffman/huffman.py
+++ Huffman/huffman.py
@@ -1,12 +1,16 @@
 # Huffman Tree Encoder
 
 
+
 class Character():
 
     def __init__(self, char):
         self.char = char
-        self.frequency = 0
-        self.code = null
+        self.frequency = 1 # to initlize there must be 1 of a char
+        self.code = ""
+
+    def getChar(self):
+        return self.char
 
     def setCode(self, code):
         self.code = code
@@ -14,11 +18,18 @@ class Character():
     def getCode(self):
         return self.code
 
-    def newInstance(self):
+    def increment(self):
         self.frequency = self.frequency + 1
 
     def getFrequency(self):
         return self.frquency
+
+    def printOut(self):
+        print("-----Print Out-----")
+        print("Char: " + self.char)
+        print("Frequency: " + str(self.frequency))
+        print("Code: " + self.code)
+        
 
 class HuffmanTree():
 
@@ -31,12 +42,57 @@ def readText():
     try:
         inFile = open("data.txt", "r")
         fileText = inFile.read()
-        print(fileText)
+
+        # next step: get from string to list of characters
+
+        charList = []
+
+        for ch in fileText:
+            charList.append(ch)
+
+        #charList is a list of all chars
+
+        return charList                    
+                    
     except (Exception):
         print("error: file not found")
 
+        return null
+
+def generateCharacterList(charList):
+    characterList = [] # list of Characters
+    
+    for char in charList: # cycle through characters in text files
+        
+        if len(characterList) != 0: # for nonempty Characterlists
+            isContained = False
+            
+            for charac in characterList: # cycle through CharacterList
+                if char == charac.getChar(): # if char from txt alreadt exists
+                    charac.increment() # incrememnt frequency
+                    isContained = True
+                    
+            if isContained == False: # if char is 'new'
+                characterList.append(Character(char)) # create Character
+                
+        else:
+            characterList.append(Character(char)) # if empty list, add to it
+
+    
+          
+    for charac in characterList:
+        charac.printOut()
+
+    return characterList
+        
+
+
+
+
 def main():
-    readText()# read text
+    charList = readText()# read text
+    characterList = generateCharacterList(charList)
+    
 
     # generate tree
 
